d0ccb34c9a624e11467d08fdb39165874df2200b
jakubG78 <jakub.gora@wp.pl>
2021-02-18 12:31:29 +0100

    commit for p8.03

--- part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,17 +5,62 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int firstContainer = 0;
+        int secondContainer = 0;
 
         while (true) {
+            String command = "";
+            int amount = 0;
+            System.out.println("First: " + firstContainer + "/100");
+            System.out.println("Second: " + secondContainer + "/100");
             System.out.print("> ");
-
             String input = scan.nextLine();
-            if (input.equals("quit")) {
+            System.out.println("");
+            String[] inputPieces = input.split(" ");
+            command = inputPieces[0];
+            if (command.equals("quit")) {
                 break;
             }
+            amount = Integer.valueOf(inputPieces[1]);
+            switch (command) {
+                case "add":
+                    if (amount > 0) {
+                        firstContainer += amount;
+                    }
+                    if (firstContainer > 100) {
+                        firstContainer = 100;
+                    }
+                    break;
 
+                case "move":
+                    if (amount > 0) {
+                        if (amount <= firstContainer) {
+                            firstContainer -= amount;
+                            secondContainer += amount;
+                        } else {
+                            secondContainer += firstContainer;
+                            firstContainer = 0;
+                        }
+                    }
+
+                    if (secondContainer > 100) {
+                        secondContainer = 100;
+                    }
+                    break;
+
+                case "remove":
+                    if (amount > 0) {
+                        if (amount <= secondContainer) {
+                            secondContainer -= amount;
+                        } else {
+                            secondContainer = 0;
+                        }
+                    }
+                    break;
+
+                default:
+                    System.out.println("Unknown command. Try again");
+            }
         }
     }
-
 }
