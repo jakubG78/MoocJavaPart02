a90ead3a5296efccaf634b7b731efbc809cecb10
jakubG78 <jakub.gora@wp.pl>
2021-02-18 11:52:37 +0100

    commit for p8.02

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,23 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sumOfPositiveNumbers = 0;
+        int counter = 0;
+        while (true) {
+            int inputNumber = Integer.valueOf(scanner.nextLine());
+            if (inputNumber == 0) {
+                break;
+            } else if (inputNumber > 0) {
+                sumOfPositiveNumbers += inputNumber;
+                counter++;
+            }
+        }
+        if (counter == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            double averageOfPositiveNumbers = 1.0 * sumOfPositiveNumbers / counter;
+            System.out.println(averageOfPositiveNumbers);
+        }
 
     }
 }
