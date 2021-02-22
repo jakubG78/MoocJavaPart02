1bf2802554d6598091f96604bbdfffd9c34019d4
jakubG78 <jakub.gora@wp.pl>
2021-02-22 21:57:05 +0100

    commit for p08.06

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -4,6 +4,11 @@ import java.util.HashMap;
 public class Nicknames {
 
     public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("matthew","matt");
+        hashMap.put("michael", "mix");
+        hashMap.put("arthur", "artie");
+        System.out.println(hashMap.get("matthew"));
         // Do the operations required here!
     }
 
