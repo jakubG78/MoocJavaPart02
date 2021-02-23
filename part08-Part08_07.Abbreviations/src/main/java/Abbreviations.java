827c61f1d12831921f5f8a7d8b01416eac427e3c
jakubG78 <jakub.gora@wp.pl>
2021-02-23 20:26:44 +0100

    commit for p8.07

---
+++ part08-Part08_07.Abbreviations/src/main/java/Abbreviations.java
@@ -0,0 +1,28 @@
+
+import java.util.HashMap;
+
+public class Abbreviations {
+
+    private HashMap<String, String> abbreviations;
+
+    public Abbreviations() {
+        this.abbreviations = new HashMap<>();
+    }
+
+    public void addAbbreviation(String abbreviation, String explanation) {
+        if(this.hasAbbreviation(abbreviation)){
+            System.out.println("This entry is already in database!");
+        } else {
+            abbreviations.put(abbreviation, explanation);
+        }
+    }
+    
+    public boolean hasAbbreviation(String abbreviation) {
+        return abbreviations.containsKey(abbreviation);
+    }
+    
+    public String findExplanationFor(String abbreviation){
+        return this.abbreviations.get(abbreviation);
+    }
+
+}
