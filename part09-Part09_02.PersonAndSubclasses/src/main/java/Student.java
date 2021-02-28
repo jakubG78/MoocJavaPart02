3fbc5375428d06ac3ee8d1dcd7672655b7d73e98
jakubG78 <jakub.gora@wp.pl>
2021-02-28 22:26:55 +0100

    commit for p9.02

---
+++ part09-Part09_02.PersonAndSubclasses/src/main/java/Student.java
@@ -0,0 +1,23 @@
+
+public class Student extends Person {
+
+    private int studyCredits;
+
+    public Student(String name, String adress) {
+        super(name, adress);
+        this.studyCredits = 0;
+    }
+
+    public void study() {
+        this.studyCredits++;
+    }
+
+    public int credits() {
+        return this.studyCredits;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\n  Study credits " + this.studyCredits;
+    }
+}
