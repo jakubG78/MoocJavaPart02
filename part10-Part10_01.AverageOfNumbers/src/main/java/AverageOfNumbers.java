31d817e45e43133821d01c7182a7d43105ca6d53
jakubG78 <jakub.gora@wp.pl>
2021-03-15 21:42:12 +0100

    commit for p10.01

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,7 +6,19 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Write your program here
-
+        ArrayList<String> input = new ArrayList<>();
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while (true) {
+            String row = scanner.nextLine();
+            if (row.equals("end")) {
+                break;
+            }
+            input.add(row);
+        }
+        double averageOfNumbers = input.stream()
+                .mapToInt(t -> Integer.valueOf(t))
+                .average()
+                .getAsDouble();
+        System.out.println("average of the numbers: " + averageOfNumbers);
     }
 }
