7c46bffea4fad4f3f00c2ffe161b534999039c9c
jakubG78 <jakub.gora@wp.pl>
2021-03-15 23:36:54 +0100

    commit for p10.05

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -6,6 +6,15 @@ public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<String> inputList = new ArrayList<>();
+        while (true) {
+            String row = scanner.nextLine();
+            if (row.isEmpty() || row.equals("\n")) {
+                break;
+            }
+            inputList.add(row);
+        }
+        inputList.stream()
+                .forEach(t -> System.out.println(t));
     }
 }
