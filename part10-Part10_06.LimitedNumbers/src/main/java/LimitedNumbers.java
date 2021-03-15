6ec9f7424f0aa0002cad37ae8437ef7c45fc579e
jakubG78 <jakub.gora@wp.pl>
2021-03-15 23:41:21 +0100

    commit for p10.06

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,16 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<Integer> numbers = new ArrayList<>();
+        while (true) {
+            Integer inputNumber = Integer.valueOf(scanner.nextLine());
+            if (inputNumber < 0) {
+                break;
+            }
+            numbers.add(inputNumber);
+        }
+        numbers.stream()
+                .filter(t -> t >= 1 && t <= 5)
+                .forEach(t -> System.out.println(t));
     }
 }
