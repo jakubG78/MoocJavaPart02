ef51586e95f397ba2d33833c47d2831e650624b6
jakubG78 <jakub.gora@wp.pl>
2021-03-16 17:39:41 +0100

    commit for p10.13

--- part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
+++ part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -3,10 +3,31 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class LiteracyComparison {
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        List<LiteracyEntry> literacyList = new ArrayList<>();
+        
+        try {
+            Files.lines(Paths.get("literacy.csv"))
+                    .map(row -> row.split(","))
+                    .map(parts -> new LiteracyEntry(parts[0], ageGroup, gender, country, 0, 0))
+                    .forEach(t -> System.out.println(t));
+        } catch (IOException e) {
+        
+            System.out.println("Error: " + e.getMessage());
+        }
+
+//        String string = "Adult literacy rate, population 15+ years, female (%),Zimbabwe,2015,85.28513";
+//        String[] pieces = string.split(",");
+//        // now pieces[0] equals "Adult literacy rate"
+//        // pieces[1] equals " population 15+ years"
+//        // etc.
+//
+//        // to remove whitespace, use the trim() method:
+//        pieces[2] = pieces[2].split(" ")[1].trim();
+//        System.out.println(pieces[2]);
     }
 }
---
+++ part10-Part10_13.LiteracyComparison/src/main/java/LiteracyEntry.java
@@ -0,0 +1,48 @@
+
+public class LiteracyEntry {
+
+    private String theme;
+    private String ageGroup;
+    private String gender;
+    private String country;
+    private int year;
+    private double literacyPercent;
+
+    public LiteracyEntry(String theme, String ageGroup, String gender, String country, int year, double literacyPercent) {
+        this.theme = theme;
+        this.ageGroup = ageGroup;
+        this.gender = gender;
+        this.country = country;
+        this.year = year;
+        this.literacyPercent = literacyPercent;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public String getAgeGroup() {
+        return ageGroup;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getLiteracyPercent() {
+        return literacyPercent;
+    }
+
+    @Override
+    public String toString() {
+        return this.country + " (" + year + "), " + gender + ", " + literacyPercent;
+    }
+}
