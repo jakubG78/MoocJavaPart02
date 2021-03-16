d7172dd24c9f8ea634f41e01f413762a69acfdb0
jakubG78 <jakub.gora@wp.pl>
2021-03-16 22:13:57 +0100

    commit for p10.15

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -1,5 +1,18 @@
 
-
 public class Checker {
 
+    public boolean isDayOfWeek(String string) {
+        return string.toLowerCase().matches("(mon|tue|wed|thu|fri|sat|sun)");
+    }
+
+    public boolean allVowels(String string) {
+        return string.toLowerCase().matches("(a|e|i|o|u)*");
+    }
+
+    public boolean timeOfDay(String string) {
+        if (string.length() == 8) {
+            return string.matches("([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]");
+        }
+        return false;
+    }
 }
