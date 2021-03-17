3e9a8eefa3c21c9781efff133f3c07dd582c5c28
jakubG78 <jakub.gora@wp.pl>
2021-03-17 12:37:25 +0100

    commit for p11.05

---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Player.java
@@ -0,0 +1,13 @@
+
+public class Player{
+
+    private String name;
+
+    public void play() {
+        System.out.println("play");
+    }
+
+    public void printName() {
+        System.out.println(this.name);
+    }
+}
